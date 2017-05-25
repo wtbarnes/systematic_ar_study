a91c40fef6f8c7a1a3db16cd6891f505376ced40
Will Barnes <wtb2@rice.edu>
2017-05-25 00:09:33 -0500

    torque submit scripts

---
+++ scripts/queup_intermediate_emiss.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#PBS -j oe
+#PBS -N noaa1109_tn2500_calc_emiss
+
+echo "Starting run at `date`"
+
+export XUVTOP="/usr/local/ssw/packages/chianti/dbase"
+
+PYTHON=/home/wtb2/anaconda3/envs/systematic-ar-study/bin/python
+SCRIPT_DIR=/home/wtb2/Documents/projects/systematic_ar_study/scripts/
+ROOT='/data/datadrive1/ar_forward_modeling/systematic_ar_study/'
+FREQ_DIR="noaa1109_tn2500/"
+FIELD_PATH=$ROOT$FREQ_DIR"field_checkpoint"
+EM_MODEL_PATH=$ROOT"emission_model1109_full"
+NEI_SAVE=$ROOT$FREQ_DIR"loop_nei.h5"
+EMISS_SAVE=$ROOT$FREQ_DIR"loop_emiss.h5"
+
+$PYTHON $SCRIPT_DIR"calculate_emission.py" -f $FIELD_PATH -e $EM_MODEL_PATH -m $EMISS_SAVE
+
+echo "Finished run at `date`"
---
+++ scripts/queup_low_flatten.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -j oe
+#PBS -N noaa1109_tn5000_flatten_counts
+
+echo "Starting run at `date`"
+
+export XUVTOP="/usr/local/ssw/packages/chianti/dbase"
+
+PYTHON=/home/wtb2/anaconda3/envs/systematic-ar-study/bin/python
+SCRIPT_DIR=/home/wtb2/Documents/projects/systematic_ar_study/scripts/
+ROOT='/data/datadrive1/ar_forward_modeling/systematic_ar_study/'
+FREQ_DIR="noaa1109_tn5000/"
+FIELD_PATH=$ROOT$FREQ_DIR"field_checkpoint"
+EM_MODEL_PATH=$ROOT"emission_model1109_full"
+
+$PYTHON $SCRIPT_DIR"flatten_detector_counts.py" -f $FIELD_PATH -e $EM_MODEL_PATH -d $ROOT$FREQ_DIR
+
+echo "Finished run at `date`"
