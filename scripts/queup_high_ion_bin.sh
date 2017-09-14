aa1632e63f5a43b6d5833ed27c58bf0e5df4f4cb
Will Barnes <wtb2@rice.edu>
2017-09-14 11:24:07 -0500

    PBS submit scripts

--- scripts/queup_high_ion_bin.sh
+++ scripts/queup_high_ion_bin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -j oe
-#PBS -N noaa1109_tn750_bin_counts
+#PBS -N noaa1109_tn750ion_bin_counts
 
 echo "Starting run at `date`"
 
@@ -9,10 +9,10 @@ export XUVTOP="/usr/local/ssw/packages/chianti/dbase"
 
 PYTHON=/home/wtb2/anaconda3/envs/systematic-ar-study/bin/python
 SCRIPT_DIR=/home/wtb2/Documents/projects/systematic_ar_study/scripts/
-ROOT='/data/datadrive1/ar_forward_modeling/systematic_ar_study/'
-FREQ_DIR="noaa1109_tn750/"
+ROOT='/data/datadrive2/ar_viz/systematic_ar_study/'
+FREQ_DIR="noaa1109_tn750-ion/"
 FIELD_PATH=$ROOT$FREQ_DIR"field_checkpoint"
-EM_MODEL_PATH=$ROOT"emission_model1109_full"
+EM_MODEL_PATH="/data/datadrive1/ar_forward_modeling/systematic_ar_study/emission_model1109_full"
 
 $PYTHON $SCRIPT_DIR"bin_detector_counts.py" -f $FIELD_PATH -e $EM_MODEL_PATH -d $ROOT$FREQ_DIR
 
--- scripts/queup_intermediate_bin.sh
+++ scripts/queup_intermediate_bin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -j oe
-#PBS -N noaa1109_intermediate_bin_counts
+#PBS -N noaa1109_tn2500_bin_counts
 
 echo "Starting run at `date`"
 
@@ -10,10 +10,10 @@ export XUVTOP="/usr/local/ssw/packages/chianti/dbase"
 PYTHON=/home/wtb2/anaconda3/envs/systematic-ar-study/bin/python
 SCRIPT_DIR=/home/wtb2/Documents/projects/systematic_ar_study/scripts/
 ROOT='/data/datadrive1/ar_forward_modeling/systematic_ar_study/'
-FREQ_DIR="noaa1109_intermediate_frequency/"
+FREQ_DIR="noaa1109_tn2500/"
 FIELD_PATH=$ROOT$FREQ_DIR"field_checkpoint"
 EM_MODEL_PATH=$ROOT"emission_model1109_full"
 
-$PYTHON $SCRIPT_DIR"bin_detector_counts.py" -f $FIELD_PATH -e $EM_MODEL_PATH -d $ROOT$FREQ_DIR
+$PYTHON $SCRIPT_DIR"bin_detector_counts.py" -f $FIELD_PATH -e $EM_MODEL_PATH -d $ROOT$FREQ_DIR -s 8.68e3
 
 echo "Finished run at `date`"
